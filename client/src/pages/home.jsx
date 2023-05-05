17db432bb1ad263b2bfd78a6927764503eca072b
David Herring <davidherring@Davids-MBP.home>
2023-05-04 21:43:09 -0500

    adding new york times api on home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Spinner from "../components/Spinner";
 
 function Home() {
+  // set state for news data
+  const [newsData, setNewsData] = useState(null);
+
+  // set URL for new's API
+  const URL =
+    "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=ZiUzWfcN4TX6yW807m6vUEVdMzgGPDJV";
+
   // initialize navigate
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -11,6 +18,19 @@ function Home() {
   // Gets current state to see if user is logged in or not. Takes in a function
   const { user } = useSelector((state) => state.auth);
   const { isLoading, isError, message } = useSelector((state) => state.auth);
+
+  // call News API
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setNewsData(data.results);
+        console.log(newsData);
+      })
+      .catch((err) => console.log(err));
+  }, [user]);
 
   useEffect(() => {
     if (isError) {
@@ -22,19 +42,29 @@ function Home() {
     }
   }, [user, navigate, isError, message, dispatch]);
 
-  if (isLoading) {
+  if (newsData === null) {
     return <Spinner />;
   }
+
   return (
     <>
-      <section className="heading">
-        <h1>Welcome {user && user.name}</h1>
-        <p>Your Goals: </p>
-      </section>
-
-      <section className="content"></section>
+      <h2>The New York Times</h2>
+      <div className="article-container">
+        {newsData.map((items) => (
+          <div key={items.id}>
+            <h5>{items.title}</h5>
+            <p>{items.abstract}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
 
 export default Home;
+
+//abstract
+//asset_id
+//title
+//url
+//byline
